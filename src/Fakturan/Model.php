b5ec48a30e36fbb1f325caafca89d66a330b8588
mintcore <johan@mintcore.se>
2015-01-30 14:28:50 +0100

    Fixed issue with attributes in scope

--- src/Fakturan/Model.php
+++ src/Fakturan/Model.php
@@ -2,11 +2,19 @@
 	
 namespace Fakturan;
 
-use Trucker as Trucker;
+use Trucker;
+use Trucker\Facades\UrlGenerator;
+use Trucker\Facades\Config;
+use Trucker\Facades\RequestFactory;
+use Trucker\Facades\ResponseInterpreterFactory;
+use Trucker\Facades\ErrorHandlerFactory;
+use Trucker\Facades\AuthFactory;
 
 class Model extends Trucker\Resource\Model {
-	
-  /**
+
+	public $attributes_in_scope = false;
+
+	/**
    * Getter function to access the
    * underlying attributes array for the
    * entity 
@@ -15,7 +23,30 @@ class Model extends Trucker\Resource\Model {
    */
   public function attributes()
   {
-	  return array(strtolower($this->getResourceName()) => $this->properties);
+	  if($this->attributes_in_scope)
+	  {
+		  return array(strtolower($this->getResourceName()) => $this->properties);
+	  }
+	  
+	  return parent::attributes();
   }
-	
+
+
+
+  /**
+   * Function to handle persistance of the entity across the
+   * remote API.  Function will handle either a CREATE or UPDATE
+   *
+   * @return Boolean  Success of the save operation
+   */
+  public function save()
+  {
+	  $this->attributes_in_scope = true;
+	  
+	  $result = parent::save();
+	  
+	  $this->attributes_in_scope = false;
+	  
+	  return $result;
+	 }
 }
