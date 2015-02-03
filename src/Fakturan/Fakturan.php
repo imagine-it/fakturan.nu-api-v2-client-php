00fbd46b15666d524981ed9d89a282a3cfd2de8d
Johan Tell <johan@mintcore.se>
2015-02-03 10:14:58 +0100

    Added options to let the user edit the endpoint

--- src/Fakturan/Fakturan.php
+++ src/Fakturan/Fakturan.php
@@ -1,12 +1,32 @@
 <?php
 
 namespace Fakturan;
-
+use Exception;
 use Trucker\Facades\Config as TruckerConfig;	
 
 class Fakturan {
+  
+  protected static $properties = [
+    'protocol' => 'https',
+    'domain' => 'fakturan.nu',
+    'uri' => 'api',
+    'version' => 2,
+    'sandbox' => false
+  ];
+  
+/*
+  protected static $protocol = 'https';
+  protected static $domain = 'fakturan.nu/api';
+  protected static $version = 2;
+  protected static $sandbox = false;
+*/
 
-	public static function setup($username, $password, $endpoint_url = 'https://fakturan.nu/api/v2')
+
+
+  #
+  #
+  #
+	public static function setup($username, $password)
 	{
 		TruckerConfig::set('auth.driver', 'basic');
 		TruckerConfig::set('auth.basic.username', $username);
@@ -14,18 +34,46 @@ class Fakturan {
 		
 		
 		# Setup config for fakturan
-		TruckerConfig::set('request.base_uri', $endpoint_url);
+		self::set_endpoint();
 		TruckerConfig::set('request.http_method_param', '_method');
 		
 		# Setup for query conditions
 		TruckerConfig::set('query_condition.driver', 'get_plain_params');
 
 	}
-	
-	public static function set_environment($environment = 'production')
+
+	#
+	#
+	#
+	public static function set($property, $value)
 	{
-    # Use the sandboxed environment
-  	TruckerConfig::set('request.base_uri', 'http://0.0.0.0:3000/api/v2');
+    if(!isset(self::$properties[$property])) throw new Exception("The property `$property` is not a valid setting");
+    
+  	self::$properties[$property] = $value;  	
+  	self::set_endpoint();
+	}
+	
+	#
+	#
+	#
+	public static function get($property)
+	{
+  	return self::$properties[$property];
+	}
+	
+	#
+	#
+	#
+	private static function set_endpoint()
+	{
+  	$url = self::$properties['protocol'].'://';
+  	
+  	# Add sandbox to endpoint if we want to use sandbox
+  	if(self::$properties['sandbox']) $url .= 'sandbox.';	
+  	
+  	$url .= self::$properties['domain'].'/'.self::$properties['uri'].'/v'.self::$properties['version'];
+  	
+  	TruckerConfig::set('request.base_uri', $url);  	
 	}
 	
 }
