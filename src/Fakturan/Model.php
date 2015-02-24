12830691b10350c2957582f5561834a2abe3c707
mintcore <johan@mintcore.se>
2015-02-24 10:56:51 +0100

    Removed unnecesarry code. Should be coupled with #2.

--- src/Fakturan/Model.php
+++ src/Fakturan/Model.php
@@ -11,25 +11,6 @@ use Trucker\Facades\ErrorHandlerFactory;
 use Trucker\Facades\AuthFactory;
 
 class Model extends Trucker\Resource\Model {
-
-	/**
-	 * Function to handle persistance of the entity across the
-	 * remote API.	Function will handle either a CREATE or UPDATE
-	 *
-	 * @return Boolean	Success of the save operation
-	 */
-	public function save()
-	{
-		$this->attributes_in_scope = true;
-		
-		$result = parent::save();
-		
-		$this->attributes_in_scope = false;
-		 
-		return $result;
-	 
-	}
-	 
 	 
 	/**
 	 * Returns attributes as json
