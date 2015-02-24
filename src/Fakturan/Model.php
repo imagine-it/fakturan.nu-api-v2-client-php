65d818c326242704e64c94702f4d384d25375845
mintcore <johan@mintcore.se>
2015-02-24 10:43:30 +0100

    Fixes #1: Added toJson function on model

--- src/Fakturan/Model.php
+++ src/Fakturan/Model.php
@@ -15,38 +15,48 @@ class Model extends Trucker\Resource\Model {
 	public $attributes_in_scope = false;
 
 	/**
-   * Getter function to access the
-   * underlying attributes array for the
-   * entity 
-   * 
-   * @return arrayhttpStatusError
-   */
-  public function attributes()
-  {
-	  if($this->attributes_in_scope)
-	  {
-		  return array(strtolower($this->getResourceName()) => $this->properties);
-	  }
-	  
-	  return parent::attributes();
-  }
+	 * Getter function to access the
+	 * underlying attributes array for the
+	 * entity 
+	 * 
+	 * @return arrayhttpStatusError
+	 */
+	public function attributes()
+	{
+		if($this->attributes_in_scope)
+			return array(strtolower($this->getResourceName()) => $this->properties);
+		
+		return parent::attributes();
+	}
 
 
 
-  /**
-   * Function to handle persistance of the entity across the
-   * remote API.  Function will handle either a CREATE or UPDATE
-   *
-   * @return Boolean  Success of the save operation
-   */
-  public function save()
-  {
-	  $this->attributes_in_scope = true;
-	  
-	  $result = parent::save();
-	  
-	  $this->attributes_in_scope = false;
-	  
-	  return $result;
-	 }
+	/**
+	 * Function to handle persistance of the entity across the
+	 * remote API.	Function will handle either a CREATE or UPDATE
+	 *
+	 * @return Boolean	Success of the save operation
+	 */
+	public function save()
+	{
+		$this->attributes_in_scope = true;
+		
+		$result = parent::save();
+		
+		$this->attributes_in_scope = false;
+		 
+		return $result;
+	 
+	}
+	 
+	 
+	/**
+	 * Returns attributes as json
+	 *
+	 * @return json attributes as json
+	 */	
+	public function toJson()
+	{
+		return json_encode($this->attributes());
+	}	
 }
